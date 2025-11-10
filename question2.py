9411b2b8506814a8c90f09f17136475dc02eff26
Nigel Douglas <ndouglas@cloudsmith.io>
2025-11-10 17:39:47 +0000

    Update question2.py

--- question2.py
+++ question2.py
@@ -141,8 +141,8 @@ import sys
 PASSWORD = "MAL-2022-7441"
 
 def download_reward():
-    reward_url = "https://raw.githubusercontent.com/ndouglas-cloudsmith/offsite-scripts/refs/heads/main/reward3.txt"
-    save_as = "reward3.txt"
+    reward_url = "https://raw.githubusercontent.com/ndouglas-cloudsmith/offsite-scripts/refs/heads/main/reward2.txt"
+    save_as = "reward2.txt"
     try:
         print("\n📥 Downloading your reward file...")
         urllib.request.urlretrieve(reward_url, save_as)
